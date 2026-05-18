5f8aa031d43ed2a59d8709b3ac8bb7fd47fe2bfa
Salma Ja <sosajaafar240@gmail.com>
2026-05-18 06:37:29 -0800

    Salma's Edits

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,30 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
     path("api/", include("apps.core.urls")),
     path("api/accounts/", include("apps.accounts.urls")),
     path("api/catalog/", include("apps.catalog.urls")),
     path("api/cart/", include("apps.cart.urls")),
     path("api/orders/", include("apps.orders.urls")),
+
+    path(
+        "api/schema/",
+        SpectacularAPIView.as_view(),
+        name="schema",         
+    ),
+
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(
+            url_name="schema"
+        ),
+        name="swagger-ui",     
+    ),
 ]
